c9adc692d4ee35389702ab013f8828c57c0e5c3e
Farhan Khan <145786270+farhankhan078@users.noreply.github.com>
2024-03-27 16:58:19 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,56 @@
+let allMusic = [
+    {
+        name: "Sari me Patola",
+        artist: "Ajay Hooda",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Left - Right",
+        artist: "Ajay Hooda",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Solid Body",
+        artist: "Ajay & Anjali",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Maharani",
+        artist: "Arpit Bala",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Jale 2",
+        artist: "Sapna Chaudhary",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "True Stories",
+        artist: "AP Dhillon",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Apa Fer Milange",
+        artist: "Savi Kahlon",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Nase c Chadh Gyi",
+        artist: "Arijit and Vishal",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "One Love",
+        artist: "Shubh",
+        img: "music-10",
+        src: "music-10"
+    },
+];
